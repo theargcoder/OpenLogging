be60bf38566a114b31df26c5ffa4b592c6de8f5e
Lucca Labattaglia <lucca.labattaglia@gmail.com>
2025-11-21 00:04:30 -0600

    Alpha 0.1.0 Basis are kinda donegit log | grep Alpha

--- OpenLogging.hpp
+++ OpenLogging.hpp
@@ -1,10 +1,8 @@
 #pragma once
 
 #include <cstddef>
-#include <fstream>
-#include <memory>
+#include <iostream>
 #include <string.h>
-#include <string>
 
 #ifdef OPENLOGGING_FORMATTING_CHAR_BEG
 
@@ -46,31 +44,98 @@
 #define OPENLOGGING_FORMATTING_CHAR_END '}'
 #endif
 
+#ifndef OPENLOGGING_BOOL_VALUE_TRUE_COLOR
+#define OPENLOGGING_BOOL_VALUE_TRUE_COLOR 64
+#endif
+
+#ifndef OPENLOGGING_BOOL_VALUE_FALSE_COLOR
+#define OPENLOGGING_BOOL_VALUE_FALSE_COLOR 91
+#endif
+
 namespace OpenLogging
 {
 constexpr char __open__ = OPENLOGGING_FORMATTING_CHAR_BEG;
 constexpr char __close__ = OPENLOGGING_FORMATTING_CHAR_END;
+constexpr static uint8_t __color_bool_value_true__ = OPENLOGGING_BOOL_VALUE_TRUE_COLOR;
+constexpr static uint8_t __color_bool_value_false__ = OPENLOGGING_BOOL_VALUE_FALSE_COLOR;
+constexpr static const char *__ansi_begin__ = "\033[";
+constexpr static const char *__ansi_end__ = "m";
+constexpr static const char *__ansi_reset__ = "\033[0m";
 
 template <size_t N>
-consteval bool
-validate_string (const char (&str)[N])
+constexpr bool validate_string (const char (&str)[N])
 {
   int A = 0, B = 0;
   for (size_t i = 0; i < N - 1; ++i) // ignore null terminator
-    {
-      if (str[i] == __open__)
-        ++A;
-      if (str[i] == __close__)
-        ++B;
-    }
+  {
+    if (str[i] == __open__)
+      ++A;
+    if (str[i] == __close__)
+      ++B;
+  }
   return A == B;
 }
 
-template <typename... Types, size_t N>
-consteval void
-log (const char (&str)[N], Types... args)
+struct __valid_string_format__
 {
-  if (!validate_string (str))
-    throw "Invalid formatted string, You have extra or missing formatting delimiters check the string please";
+  const char *str;
+  size_t N;
+
+  template <size_t M>
+  consteval __valid_string_format__ (const char (&str)[M]) : str (str), N (M)
+  {
+    if (!validate_string (str))
+      throw "Invalid formatted string, You have extra or missing formatting delimiters check the string please";
+  }
+};
+
+template <typename T>
+static std::string __to_string_into_buff__ (T &var)
+{
+  static std::string __buffer__;
+  __buffer__.reserve (4096);
+
+  size_t i = 0;
+  if constexpr (std::is_same_v<T, bool>)
+  {
+    if (var)
+      __buffer__ += __ansi_begin__ + std::to_string (__color_bool_value_true__) + __ansi_end__ + "true";
+    else
+      __buffer__ += __ansi_begin__ + std::to_string (__color_bool_value_false__) + __ansi_end__ + "false";
+  }
+
+  return __buffer__;
 }
+
+template <typename... Types>
+void log (const __valid_string_format__ fmt, Types... args)
+{
+  const auto *str = fmt.str;
+  const auto N = fmt.N;
+  (
+      [&] (auto __arg)
+      {
+        for (size_t i = 0, argc = 0; i < N; i++)
+        {
+          if (str[i] != __open__ && str[i] != __close__)
+          {
+            std::cout << str[i];
+          }
+          else if (str[i] == __open__)
+          {
+            i++;                     // WE skip the formatting delimiter
+            if (str[i] == __close__) // direct print if its just followed by a closed delim (no formatting args)
+              std::cout << __to_string_into_buff__ (__arg);
+          }
+          else
+          {
+            argc++;
+          }
+        }
+      }(args),
+      ...);
+
+  std::cout << "\n";
 }
+
+} // namespace OpenLogging
