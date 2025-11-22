8a8ecaca1171721c66d49737b2ce2c0e82096d7e
Lucca Labattaglia <lucca.labattaglia@gmail.com>
2025-11-21 18:53:47 -0600

    Alpha 0.1.1 Basis are improving for sure

--- test.cpp
+++ test.cpp
@@ -5,6 +5,19 @@ int main (int argc, char *argv[])
   OpenLogging::log ("HELlO {}");
   // COMPILE TIME ERROR
   // OpenLogging::log ("HELlO }");
+  void *a;
+  int b = 9;
+  a = &b;
+
+  OpenLogging::log ("hello {}", a);
+  OpenLogging::log ("hello {}", nullptr);
   OpenLogging::log ("hello {}", true);
+  OpenLogging::log ("hello {}", 1);
+  OpenLogging::log ("hello {}", 1.0);
+  OpenLogging::log ("hello {}", 1.0f);
+  OpenLogging::log ("hello {}", "const char *");
+  OpenLogging::log ("hello {}", const_cast<char *> ("const char *"));
+  OpenLogging::log ("hello {}", std::string ("std::string"));
+
   return 0;
 }
