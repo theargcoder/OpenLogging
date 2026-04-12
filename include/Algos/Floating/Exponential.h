a601922f91bfaa86075f83160743538a886a7d36
Lucca Labattaglia <lucca.labattaglia@gmail.com>
2026-04-12 11:32:26 -0600

    HA i knew i didnt need that horrible rounding from RYU

--- include/Algos/Floating/Exponential.h
+++ include/Algos/Floating/Exponential.h
@@ -18,38 +18,6 @@
 
 namespace Helpers::Numeric::Floating::ExponentialNotation
 {
-  static inline uint32_t pow5Factor(uint64_t value)
-  {
-    const uint64_t m_inv_5 = 14757395258967641293u; // 5 * m_inv_5 = 1 (mod 2^64)
-    const uint64_t n_div_5 = 3689348814741910323u;  // #{ n | n = 0 (mod 2^64) } = 2^64 / 5
-    uint32_t count = 0;
-    for(;;)
-    {
-      assert(value != 0);
-      value *= m_inv_5;
-      if(value > n_div_5)
-        break;
-      ++count;
-    }
-    return count;
-  }
-
-  // Returns true if value is divisible by 5^p.
-  static inline bool multipleOfPowerOf5(const uint64_t value, const uint32_t p)
-  {
-    // I tried a case distinction on p, but there was no performance difference.
-    return pow5Factor(value) >= p;
-  }
-
-  // Returns true if value is divisible by 2^p.
-  static inline bool multipleOfPowerOf2(const uint64_t value, const uint32_t p)
-  {
-    assert(value != 0);
-    assert(p < 64);
-    // __builtin_ctzll doesn't appear to be faster here.
-    return (value & ((1ULL << p) - 1)) == 0;
-  }
-
   template <typename T>
     requires std::is_floating_point_v<T> && (Helpers::Templating::Assert::at_most_64_bit_double_radix_2<T>())
   static auto ToStrCharArray(const T &input, int PRECISION = Constants::Tables::Floating<T>::MAX_DIGITS10)
@@ -152,24 +120,9 @@ namespace Helpers::Numeric::Floating::ExponentialNotation
       }
       else
       {
-        const auto rem_exp = PRECISION - exp_base_10_int;
-        const auto required_twos = -(exp - Floating::MANTISSA_BITS - Floating::EXPONENT_BIAS) - rem_exp;
-        bool trailingZeros = required_twos <= 0 || (required_twos < 60 && multipleOfPowerOf2(mantissa, (uint32_t)required_twos));
-        if(rem_exp < 0)
-        {
-          const int32_t requiredFives = -rem_exp;
-          trailingZeros = trailingZeros && multipleOfPowerOf5(mantissa, (uint32_t)requiredFives);
-        }
-        if(trailingZeros)
-        {
-          // Apply Round-Ties-To-Even on the LAST VISIBLE DIGIT.
-          const auto last_digit = digits_10 % 10;
-          if(last_digit & 1U)
-          {
-            digits_10++;
-          }
-        }
-        else
+        // Apply Round-Ties-To-Even on the LAST VISIBLE DIGIT.
+        const auto last_digit = digits_10 % 10;
+        if(last_digit & 1U)
         {
           digits_10++;
         }
